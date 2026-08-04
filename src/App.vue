e7e4b7f673240f6ccca717bbf96228dfd7a9312e
jdimichele <justindimichele@gmail.com>
2026-08-04 16:35:47 -0700

    Using background through component rather than directly in HTML

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<template><div>test</div></template>
+<template>
+  <StarsBackground />
+  <div>
+    <router-view class=" relative z-10">test</router-view>
+  </div>
+</template>
 
-<script>
-import BaseCard from './components/BaseCard.vue'
+<script setup>
+import StarsBackground from './components/ui/StarsBackground.vue'
 </script>
